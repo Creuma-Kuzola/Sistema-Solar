054585fd09c1d1af5eb86bf5a24780fd66d111ea
Creuma Kuzola <creumakuzola@gmail.com>
2020-11-17 00:44:20 +0100

    Actualizações na classe Painel

--- PainelSistemaSolar.java
+++ PainelSistemaSolar.java
@@ -8,10 +8,7 @@ package sistema.solar;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JPanel;
 
 /**
@@ -21,11 +18,12 @@ import javax.swing.JPanel;
 public class PainelSistemaSolar extends JPanel implements Runnable {
     
     private final Thread thread;
-    private float earthDegree = 0;
+    private int terraAngulo = 0;
+    private int luaAngulo = 0;
     
     public PainelSistemaSolar(){
     
-        thread = new Thread(this, "Rotate Scare");
+        thread = new Thread(this);
         thread.start();
         
     }
@@ -37,26 +35,28 @@ public class PainelSistemaSolar extends JPanel implements Runnable {
         Graphics2D g2d = (Graphics2D) g;
       
         this.setBackground(new Color(0,0,0));
-        Ellipse2D sol = new Ellipse2D.Double(280, 130, 80, 80);
-        Ellipse2D terra = new Ellipse2D.Double(390, 180, 40, 40);
-        Ellipse2D lua = new Ellipse2D.Double(440,230, 10, 10);
         
+        Ellipse2D sol = new Ellipse2D.Double(280, 130, 80, 80);
+        // Posição da terra é o final da posição do sol + 30.
+        Ellipse2D terra = new Ellipse2D.Double(390, 210, 40, 40);
+        // Posição do sol é o final da posição da terra + 80
+        Ellipse2D lua = new Ellipse2D.Double(430, 250, 10, 10);
+        
+        //Sol
         g2d.setPaint(new Color(235,210,29));
         g2d.fill(sol);
         
-       
-       // g2d.rotate(Math.PI / 8, 280/2, 130/2);
-        g2d.setPaint(new Color(255,255,255));
-        g2d.rotate(Math.toRadians(earthDegree), 140, 65);
+        // Terra
+        g2d.setPaint(new Color(192,192,192));
+        // 320 e 170 são os valores do centro da terra.
+        g2d.rotate(Math.toRadians(terraAngulo), 320,170);
         g2d.fill(terra);
-       // g2d.fill(lua);
         
-        
-      //  AffineTransform at = new AffineTransform();
-        //at.setToRotation(Math.PI / 6, 100,100);
-       // at.translate(100, 0);
-    
-       
+        // Lua
+        g2d.setPaint(new Color(255,255,255));
+        g2d.rotate(Math.toRadians(luaAngulo), terra.getCenterX(), terra.getCenterY());
+        g2d.fill(lua);
+     
     }
 
     @Override
@@ -64,12 +64,17 @@ public class PainelSistemaSolar extends JPanel implements Runnable {
         
         try {
             
-            this.earthDegree = (float) (this.earthDegree >= 360 ? 0 : 0.02 + this.earthDegree);
-            Thread.sleep(22);
-            super.repaint();
+            while(true)
+            {
+
+                this.terraAngulo = (int) (this.terraAngulo >= 360 ? 0 : 1 + this.terraAngulo);
+                this.luaAngulo = (int) (this.luaAngulo >= 360 ? 0 : 1 + this.luaAngulo);
+                Thread.sleep(10);
+                super.repaint();
             
+            }
         } catch (InterruptedException ex) {
-            System.out.println("Bla bla bla");
+            System.out.println("ERRO");
         }
                 
     }
